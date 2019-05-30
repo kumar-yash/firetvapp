8be9a9634ab51ae55dd3722d2e499874bf5095bb
Yash Kumar <yash.kumar@CF-LP00261.local>
2019-05-31 00:50:44 +0530

    Updates

---
+++ precache-manifest.16f5a0d99cdab7f66937553c4d9d7657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c5b352126624deb03784d2f27306b61",
+    "url": "/firetvapp/index.html"
+  },
+  {
+    "revision": "081ace4de692af01adef",
+    "url": "/firetvapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/firetvapp/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "081ace4de692af01adef",
+    "url": "/firetvapp/static/js/main.27936239.chunk.js"
+  },
+  {
+    "revision": "3ac41a83b149be57d23b",
+    "url": "/firetvapp/static/js/runtime~main.b988a6e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/firetvapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
