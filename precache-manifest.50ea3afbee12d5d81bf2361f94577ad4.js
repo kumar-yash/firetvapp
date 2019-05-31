805f224268147d0453e2b103b6b574dcd5b2335b
Yash Kumar <yash.kumar@CF-LP00261.local>
2019-05-31 23:53:05 +0530

    Updates

---
+++ precache-manifest.50ea3afbee12d5d81bf2361f94577ad4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016c04b9d3a2a4cf6e3e1979d62a4da1",
+    "url": "/firetvapp/index.html"
+  },
+  {
+    "revision": "21152f825e501f58fafe",
+    "url": "/firetvapp/static/css/main.1e4b2585.chunk.css"
+  },
+  {
+    "revision": "4e573da3437d7fdf2457",
+    "url": "/firetvapp/static/js/2.910f9ef9.chunk.js"
+  },
+  {
+    "revision": "21152f825e501f58fafe",
+    "url": "/firetvapp/static/js/main.7a01f4fd.chunk.js"
+  },
+  {
+    "revision": "3ac41a83b149be57d23b",
+    "url": "/firetvapp/static/js/runtime~main.b988a6e5.js"
+  },
+  {
+    "revision": "6c13ecddc1346f7be2c62d562e14d85b",
+    "url": "/firetvapp/static/media/l11.6c13ecdd.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/firetvapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/firetvapp/precache-manifest.93a380dc01f7f391e9e13e9fbf32a649.js"
+  "/firetvapp/precache-manifest.50ea3afbee12d5d81bf2361f94577ad4.js"
 );
 
 self.addEventListener('message', (event) => {
